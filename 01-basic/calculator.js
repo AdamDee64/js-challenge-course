1bf0a0b320a06432c1d7727f753db6d1d11ba443
Adam <deea7243@gmail.com>
2023-09-10 06:33:47 -0400

    added files

---
+++ 01-basic/calculator.js
@@ -0,0 +1,28 @@
+function calculate(a, b, s) {
+  switch (s) {
+    case '+':
+      return a + b
+      break
+    case '-':
+      return a - b
+      break
+    case '/':
+      return a / b
+      break
+    case '*':
+      return a * b
+      break
+    case '%':
+      return a % b
+      break
+    default:
+      return 'invalid input'
+  }
+}
+
+console.log(calculate(2, 3, '+')) // 5
+console.log(calculate(10, 5, '-')) // 5
+console.log(calculate(9, 3, '/')) // 3
+console.log(calculate(2, 4, '*')) // 8
+console.log(calculate(10, 3, '%')) // 1
+console.log(calculate(6, 7, 'x')) // invalid
---
+++ 01-basic/count-occurrences.js
@@ -0,0 +1,43 @@
+// count the occurrences of a char within a string
+
+
+// case insensitive version
+function countOccurrences(str, char) {
+  let count = 0
+  let b = char.charCodeAt(0)
+
+  for(let i = 0; i < str.length; i++){
+    let a = str.charCodeAt(i)
+
+    // check uppercase match
+    if (b >= 65 && b <= 90){
+      if (b == a || b + 32 == a){
+        count++
+      }
+
+    //check lowercase match
+    } else if (b >= 97 && b <= 122){
+      if (b == a || b - 32 == a){
+        count++
+      }
+
+    // check all other characters
+    } else {
+      if (b == a){
+        count++
+      }
+    }
+  }
+
+  return count
+}
+
+var string1 = 'HELLO world!'
+  
+console.log(countOccurrences(string1, 'l')) // 3
+console.log(countOccurrences(string1, 'L')) // 3
+console.log(countOccurrences(string1, 'z')) // 0
+console.log(countOccurrences(string1, 'e')) // 1
+console.log(countOccurrences(string1, 'o')) // 2
+console.log(countOccurrences(string1, '!')) // 1
+console.log(countOccurrences(string1, '?')) // 0
